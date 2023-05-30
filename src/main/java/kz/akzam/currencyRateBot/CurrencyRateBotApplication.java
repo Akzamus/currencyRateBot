1e26a4af26d56931ff723b04c1510e43d20a851c
Akzam Khamitov <makoron3002@gmail.com>
2023-05-30 12:07:20 +0600

    Initial commit

---
+++ src/main/java/kz/akzam/currencyRateBot/CurrencyRateBotApplication.java
@@ -0,0 +1,13 @@
+package kz.akzam.currencyRateBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/akzam/currencyRateBot/CurrencyRateBotApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.akzam.currencyRateBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
